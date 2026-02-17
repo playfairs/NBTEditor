1e5d6651b6962124486004ae01918ff5ac5d05f8
playfairs <root@playfairs.cc>
2026-02-16 21:41:17 -0600

    formatting: flake.nix

--- flake.nix
+++ flake.nix
@@ -6,19 +6,25 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        
+
         emscripten = pkgs.emscripten;
-        
+
         nativeBuildInputs = with pkgs; [
           emscripten
           gnumake
           python3
         ];
-        
+
         runtimeInputs = with pkgs; [
           python3
         ];
@@ -26,7 +32,7 @@
       {
         devShells.default = pkgs.mkShell {
           buildInputs = nativeBuildInputs ++ runtimeInputs;
-          
+
           shellHook = ''
             serve-nbteditor() {
               echo "Starting NBTEditor server at http://localhost:8080"
@@ -38,16 +44,16 @@
         packages.default = pkgs.stdenv.mkDerivation {
           pname = "nbteditor";
           version = "1.0.0";
-          
+
           src = ./.;
-          
+
           nativeBuildInputs = nativeBuildInputs;
-          
+
           buildPhase = ''
             source ${emscripten}/emsdk_env.sh
             make build
           '';
-          
+
           installPhase = ''
             mkdir -p $out
             cp -r web-app/* $out/
@@ -56,33 +62,35 @@
 
         apps.default = {
           type = "app";
-          program = toString (pkgs.writeShellScript "serve-nbteditor" ''
-            cd web-app
-            echo "Starting NBTEditor server at http://localhost:8080"
-            ${pkgs.python3}/bin/python3 -m http.server 8080
-          '');
+          program = toString (
+            pkgs.writeShellScript "serve-nbteditor" ''
+              cd web-app
+              echo "Starting NBTEditor server at http://localhost:8080"
+              ${pkgs.python3}/bin/python3 -m http.server 8080
+            ''
+          );
         };
 
         formatter = pkgs.nixfmt;
-        
+
         packages.desktop = pkgs.stdenv.mkDerivation {
           pname = "nbteditor-desktop";
           version = "1.0.0";
-          
+
           src = ./.;
-          
+
           nativeBuildInputs = with pkgs; [
             nodejs
             npm
             pkgs.electron
             pkgs.electron-builder
           ];
-          
+
           buildPhase = ''
             npm ci
             npm run build
           '';
-          
+
           installPhase = ''
             mkdir -p $out/bin
             mkdir -p $out/share/nbteditor
@@ -94,5 +102,6 @@
             chmod +x $out/bin/nbteditor
           '';
         };
-      });
+      }
+    );
 }
